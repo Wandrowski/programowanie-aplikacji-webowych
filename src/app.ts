c742c4abc955b9cabd62d370e727744345ff6f71
Wandrowski <72198101+Wandrowski@users.noreply.github.com>
2021-04-14 10:09:53 +0200

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import { Horse } from './Horse';
+import { Snake } from './Snake';
+import { Animal } from './Animal';
+let sam = new Snake("Sammy the Python");
+  let tom: Animal = new Horse("Tommy the Palomino");
+
+  sam.move();
+  tom.move(34);
